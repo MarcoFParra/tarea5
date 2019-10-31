b513e27a1aaf84978c54500142a954f8c365dc9a
marco <marco_an_fi_pa@hotmail.com>
2019-10-31 11:58:20 -0600

    se implemento moore

--- app/src/main/java/mp/tareas/tarea5/MainActivity.java
+++ app/src/main/java/mp/tareas/tarea5/MainActivity.java
@@ -142,8 +142,9 @@ public class MainActivity extends AppCompatActivity {
                 public boolean onQueryTextChange(String newText) {
                     Log.i("onQueryTextChange", newText);
                     if (newText.length()>0)
-                    buscarTexto(newText);
-                    else {
+                    //buscarTexto(newText);
+                    search(editText.getText().toString(), newText);
+                        else {
                         ((TextView)findViewById(R.id.txt_res)).setText("Resultados:");
                         editText.setText(editText.getText().toString());
                     }
@@ -160,6 +161,95 @@ public class MainActivity extends AppCompatActivity {
             searchView.setOnQueryTextListener(queryTextListener);
         }
         return true;
+    }
+
+
+    static int NO_OF_CHARS = 256;
+
+    //A utility function to get maximum of two integers
+    static int max (int a, int b) { return (a > b)? a: b; }
+
+    //The preprocessing function for Boyer Moore's
+    //bad character heuristic
+    static void badCharHeuristic( String str, int size,int badchar[])
+    {
+        int i;
+
+        // Initialize all occurrences as -1
+        for (i = 0; i < NO_OF_CHARS; i++)
+            badchar[i] = -1;
+
+        // Fill the actual value of last occurrence
+        // of a character
+        for (i = 0; i < size; i++)
+            badchar[(int) str.charAt(i)] = i;
+    }
+
+    /* A pattern searching function that uses Bad
+    Character Heuristic of Boyer Moore Algorithm */
+    void search( String txt,  String pat)
+    {
+        txt = txt + " ";
+        resultTexts.clear();
+        text = new SpannableString(editText.getText().toString());
+
+        int m = pat.length();
+        int n = txt.length();
+
+        int badchar[] = new int[NO_OF_CHARS];
+
+      /* Fill the bad character array by calling
+         the preprocessing function badCharHeuristic()
+         for given pattern */
+        badCharHeuristic(pat, m, badchar);
+
+        int s = 0;  // s is shift of the pattern with
+        // respect to text
+        while(s <= (n - m))
+        {
+            int j = m-1;
+
+          /* Keep reducing index j of pattern while
+             characters of pattern and text are
+             matching at this shift s */
+            while(j >= 0 && pat.charAt(j) == txt.charAt(s+j))
+                j--;
+
+          /* If the pattern is present at current
+             shift, then index j will become -1 after
+             the above loop */
+            if (j < 0)
+            {
+                System.out.println("Patterns occur at shift = " + s);
+                resultTexts.add(new ResultText(s, s + ( (s+m < n)? m-badchar[txt.charAt(s+m)]  -1: 0)) );
+                text.setSpan(new ForegroundColorSpan(Color.BLUE), s, s + ( (s+m < n)? m-badchar[txt.charAt(s+m)] -1 : 0), 0);
+
+
+              /* Shift the pattern so that the next
+                 character in text aligns with the last
+                 occurrence of it in pattern.
+                 The condition s+m < n is necessary for
+                 the case when pattern occurs at the end
+                 of text */
+                s += (s+m < n)? m-badchar[txt.charAt(s+m)] : 1;
+
+            }
+
+            else
+              /* Shift the pattern so that the bad character
+                 in text aligns with the last occurrence of
+                 it in pattern. The max function is used to
+                 make sure that we get a positive shift.
+                 We may get a negative shift if the last
+                 occurrence  of bad character in pattern
+                 is on the right side of the current
+                 character. */
+                s += max(1, j - badchar[txt.charAt(s+j)]);
+        }
+
+        ((TextView)findViewById(R.id.txt_res)).setText("Resultados: " + resultTexts.size());
+        editText.setText(text);
+
     }
 
     private void buscarTexto(String busqueda) {
